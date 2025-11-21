178475f8a87d85b3d5180163a968d8d644566bde
Kunal Katariya <krkband@gmail.com>
2025-11-21 20:25:04 +0900

    work

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/soraguide_v1/',
-})
+  base: process.env.NODE_ENV === 'production' ? '/soraguide_v1/' : '/',
+});
