7d2855a4dbe3547224b213262c49afa17099c469
Kunal Katariya <krkband@gmail.com>
2025-11-21 18:54:26 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Personalize from './pages/Personalize';
 import PlanningInfo from './pages/PlanningInfo';
@@ -17,7 +17,7 @@ import './App.css';
 function App() {
   return (
     <AuthProvider>
-      <Router basename="/soraguide_v1/">
+      <Router>
         <ScrollToTop />
         <div className="app">
           <Routes>
